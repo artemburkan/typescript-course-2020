69d0e54d4dfc832354d5d23b379bc672264325d5
Artyom Burkan <artyom.burkan@gmail.com>
2020-07-22 17:20:25 +0700

    Added react-redux app

---
+++ react-redux-app/src/reducers/index.ts
@@ -0,0 +1,9 @@
+import { combineReducers } from "redux"
+import { todosReducer } from "./todos"
+import { Todo } from "../actions"
+
+export interface StoreState {
+  todos: Todo[]
+}
+
+export const reducers = combineReducers<StoreState>({ todos: todosReducer })
